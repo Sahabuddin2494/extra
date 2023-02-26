13bfa8f5c1cc170019bc3ce0d73db8c3d018e590
sahabuddin <sagorkhan83495@gmail.com>
2023-02-27 00:54:01 +0600

    erer

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,2 +1,6 @@
 var parent=document.querySelectorAll('.mid')[0];
-parent.innerHt
+var change=parent.children;
+ var first=change[0];
+ first.style.color="red";
+ first.style.backgroundColor="green";
+
